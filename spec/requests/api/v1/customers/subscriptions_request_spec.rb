678ac15bc46ac2c3a2b0a2959430ebaca4330b87
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-09-15 13:56:01 -0700

    Begin initial testing for teas

--- spec/requests/api/v1/customers/subscriptions_request_spec.rb
+++ spec/requests/api/v1/customers/subscriptions_request_spec.rb
@@ -1,12 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe 'Subscriptions', type: :request do
-  before :each do
+  it 'exhibits a customer subscription life cycle' do
+    customer_1 = create(:customer)
+    tea_1 = create(:tea)
+    subscription_1 = create(:subscription, customer: customer_1, tea: tea_1)
 
-    visit "/"
-  end
+    post api_v1_new_customer_path(customer_1)
+    customer_response = JSON.parse(response.body, symbolize_names: true)
+    post api_v1_new_tea_path(tea_1)
+    tea_response = JSON.parse(response.body, symbolize_names: true)
+    post api_v1_new_subscription_path(subscription_1)
+    subscription_response = JSON.parse(response.body, symbolize_names: true)
 
-  it '' do
 
   end
 end
