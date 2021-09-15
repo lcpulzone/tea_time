b042860b1d65556eefd31e2efb804bfa02a94d70
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-09-14 20:30:02 -0700

    Add in validation tests and validations

--- app/models/tea.rb
+++ app/models/tea.rb
@@ -1,3 +1,8 @@
 class Tea < ApplicationRecord
   has_many :subscriptions
+
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :temperature_in_degrees, presence: true
+  validates :brew_time_in_minutes, presence: true
 end
