e65249477eb80b7e25c69136e8fae20c02103eb0
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-09-14 19:41:53 -0700

    Add relationship tests to all model specs, not passing

--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Subscription, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'relationships' do
+    it { should belong_to(:tea) }
+    it { should belong_to(:customer) }
+  end
 end
+
+  # describe 'validations' do
+  #   it { should validate_presence_of(:) }
+  # end
+  #
+  # before :each do
+  #
+  # end
+  #
+  # describe 'class methods' do
+  #   describe '.' do
+  #   end
+  # end
+  #
+  # describe 'instance methods' do
+  #   describe '#' do
+  #   end
--- spec/models/tea_spec.rb
+++ spec/models/tea_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Tea, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'relationships' do
+    it { should have_many(:subscriptions) }
+  end
 end
+
+  # describe 'validations' do
+  #   it { should validate_presence_of(:) }
+  # end
+  #
+  # before :each do
+  #
+  # end
+  #
+  # describe 'class methods' do
+  #   describe '.' do
+  #   end
+  # end
+  #
+  # describe 'instance methods' do
+  #   describe '#' do
+  #   end
+  # end
