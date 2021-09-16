1e8de32c4a0f9a67a0fbb2bc9796d89dd6becbae
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-09-15 19:42:27 -0700

    Add expect's to complete test

--- spec/requests/api/v1/subscriptions_request_spec.rb
+++ spec/requests/api/v1/subscriptions_request_spec.rb
@@ -1,35 +1,60 @@
 require 'rails_helper'
 
 RSpec.describe 'Subscriptions', type: :request do
-  it 'exhibits a customer subscription life cycle' do
-    customer_1 = {
+  describe 'Create' do
+    it 'can generate a new customer, tea and subscription' do
+      customer_1 = {
         first_name: "Danilo",
         last_name: "Stark",
         email: "vincenza@example.net",
         address: "70069 Emmerich Glens, Port Kinamouth, IN 92013-6820"
       }
-    tea_1 = {
+      tea_1 = {
         title: "Shui Xian",
         description: "Green",
         temperature_in_fahrenheit: 186,
         brew_time_in_minutes: 2.0
       }
+
+      post api_v1_customers_path(customer_1)
+      customer_response = JSON.parse(response.body, symbolize_names: true)
+
+      post api_v1_teas_path(tea_1)
+      tea_response = JSON.parse(response.body, symbolize_names: true)
+
       subscription_1 = {
         title: "Breath of the Wild",
         price: 28.32,
         status: "active",
-        frequency: "Triennal"
+        frequency: "Triennal",
+        tea_id: "#{tea_response[:data][:id]}",
+        customer_id: "#{customer_response[:data][:id]}"
       }
 
-    post api_v1_customers_path(customer_1)
-    customer_response = JSON.parse(response.body, symbolize_names: true)
+      post api_v1_subscriptions_path(subscription_1)
+      subscription_response = JSON.parse(response.body, symbolize_names: true)
 
-    post api_v1_teas_path(tea_1)
-    tea_response = JSON.parse(response.body, symbolize_names: true)
-
-    post api_v1_subscriptions_path(subscription_1)
-    subscription_response = JSON.parse(response.body, symbolize_names: true)
-
-
+      expect(subscription_response.class).to eq(Hash)
+      expect(subscription_response[:data].class).to eq(Hash)
+      expect(subscription_response[:data][:attributes].size).to eq(6)
+      expect(subscription_response[:data][:attributes][:title].class).to eq(String)
+      expect(subscription_response[:data][:attributes][:price].class).to eq(Float)
+# {
+#   :data=>
+#   {
+#     :id=>"42",
+#     :type=>"subscriptions",
+#     :attributes=>
+#     {
+#       :title=>"Breath of the Wild",
+#       :price=>28.32,
+#       :status=>"active",
+#       :frequency=>"Triennal",
+#       :tea_id=>51,
+#       :customer_id=>55
+#     }
+#   }
+# }
+    end
   end
 end
