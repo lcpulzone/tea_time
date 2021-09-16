6917a39e599f6d159eb9796a78b618aaa5918898
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-09-15 20:43:13 -0700

    Update subscription route to have 'update' action

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :customers, only: [:create]
-      resources :subscriptions, only: [:create]
+      resources :subscriptions, only: [:create, :update]
       resources :teas, only: [:create]
     end
   end
