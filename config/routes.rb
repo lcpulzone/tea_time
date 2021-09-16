0a356b5a72940c35e6433418e23e50e7cebf90bd
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-09-15 22:29:52 -0700

    Add in nested resource for a customer's subscriptions

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :customers, only: [:create]
+      resources :customers, only: [:create] do
+        resources :subscriptions, only: [:index]
+      end
       resources :subscriptions, only: [:create, :update]
       resources :teas, only: [:create]
     end
