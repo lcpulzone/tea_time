6243120cc065b246653aa5328da83514648bb07b
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-09-18 18:29:33 -0700

    Nest subscription update route

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :customers, only: [:create] do
-        resources :subscriptions, only: [:index]
+        resources :subscriptions, only: [:index, :update]
       end
-      resources :subscriptions, only: [:create, :update]
+      resources :subscriptions, only: [:create]
       resources :teas, only: [:create]
     end
   end
