e87945b1c8d9c6430f2f72dcb0e13c16ceb15d10
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-10-01 18:26:32 -0700

    Add status method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::API
+  def status(marker)
+    case marker
+      when :customer_not_found
+        404
+      when :subscription_not_found
+        404
+      else
+        400
+    end
+  end
 end
